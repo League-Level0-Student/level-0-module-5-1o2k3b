f972346432c8424978a54c49ff0cfaa366ef683a
league <league@imac-18.attlocal.net>
2022-09-07 18:34:42 -0700

    yay

---
+++ src/_01_algorithms/_2_fibonacci/unnamed.java
@@ -0,0 +1,17 @@
+package _01_algorithms._2_fibonacci;
+import javax.swing.JOptionPane;
+
+public class unnamed {
+public static void main(String[] args) {
+	int secondlast = 0;
+	int last = 1;
+	for(int i = 0; i<12; i++ ) {
+		int f = secondlast + last;
+		secondlast = last;
+		last = f;
+		JOptionPane.showInputDialog(f);
+		
+	}
+	
+}
+}
