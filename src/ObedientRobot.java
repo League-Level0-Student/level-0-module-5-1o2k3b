15de74c0ec7c30bff72c2e76d49aae3fb44da582
root <root@3ab4a56ddacb>
2022-10-23 21:26:16 +0000

    committed:)

---
+++ src/ObedientRobot.java
@@ -0,0 +1,10 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class ObedientRobot {
+public static void main(String[] args) {
+Robot rob = new Robot();
+for(int i = 0; i<4; i++) {
+	
+}
+}
+}
--- src/guantlet.java
+++ src/guantlet.java
@@ -25,7 +25,37 @@ public static void main(String[] args) {
 		System.out.println(i);
 	}
 	for(int i = 2; i<101; i++) {
-		System.out.println(i);
+		if(i%2 == 0) {
+			System.out.println(i);
+		}
+		}
+	for(int i = 1; i<100; i++) {
+		if(i%2 == 1) {
+			System.out.println(i);	
+		}
+
+		
 	}
+	for(int i = 1; i<501; i++) {
+		if(i%2 == 1) {
+			System.out.println(i+"odd");
+		}
+			else {
+				System.out.println(i+"even");
+			}
+		
+		
+}
+	for(int i = 7; i<778; i++) {
+		if(i%7 == 0) {
+			System.out.println(i);	
+		}
+}
+	int x= 2011;
+	for(int i = 2011; i<2023; i++) {
+			System.out.println("in "+i+" I was "+(i-x)+" years old");	
+}
+
+
 }
 }
