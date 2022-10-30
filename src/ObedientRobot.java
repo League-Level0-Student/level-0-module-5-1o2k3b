4a0f556e47e6f755ba1f8c87dee3c7938fe78232
league <league@imac-24.attlocal.net>
2022-10-30 11:30:39 -0700

    yumyumyumyumyuymuyuyuyuyuyyuhyujmh

--- src/ObedientRobot.java
+++ src/ObedientRobot.java
@@ -1,10 +1,101 @@
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class ObedientRobot {
 public static void main(String[] args) {
 Robot rob = new Robot();
-for(int i = 0; i<4; i++) {
-	
+rob.setSpeed(100);
+rob.penDown();
+rob.setX(50);
+rob.setY(540);
+String x = JOptionPane.showInputDialog("ask my robot rob to draw a square triangle or circle");
+if(x.equals("triangle")) {
+	drawTriangle(rob, 100);
 }
+if(x.equals("square")) {
+	drawSquare(rob, 100);
+}
+if(x.equals("circle")) {
+	drawCircle(rob, 100);
+}
+}
+
+
+static void drawSquare(Robot rob, int size) {
+	for(int i = 0; i<4; i++) {
+		rob.move(size);
+		rob.turn(90);
+	}
+
+}
+static void drawCircle(Robot rob, int size) {
+	for(int i = 0; i<4; i++) {
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);			
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);		
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+		rob.move(size);
+		rob.turn(3);
+	}
+}
+static void drawTriangle(Robot rob, int size) {
+	for(int i = 0; i<3; i++) {
+		rob.move(size);
+		rob.turn(120);
+	}
+
 }
 }
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -5,23 +5,28 @@ public class HappyPet {
 	
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happynessLevel = 0;
 	public static void main(String[] args) {
+
+		
+String x = JOptionPane.showInputDialog("what type of pet do you want to get");
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
 
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
+	for(int i = 0; i<100; i++) {
 		
+	
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+			int task = JOptionPane.showOptionDialog(null, "what do you want to do to make your "+x+" happy", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "give food", "pet", "take outside" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
 
 			// 7. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+	}
 	}
 
 	// 5. Create methods to handle each of your user selections.
