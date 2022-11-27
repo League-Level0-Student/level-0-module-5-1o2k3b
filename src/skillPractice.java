60cf9afa346aaa36a203708b582f697f5d3a9d2b
league <league@imac-24.attlocal.net>
2022-11-27 11:41:51 -0800

    1234

--- src/skillPractice.java
+++ src/skillPractice.java
@@ -8,15 +8,41 @@ public static void main(String[] args) {
 	skills.skill1();
 	skills.skill2();
 	skills.skill3();
+	skills.skill4();
+	skills.skill5();
 }
-
+private void skill5() {
+String s = JOptionPane.showInputDialog("what school dp you go to");
+JOptionPane.showInputDialog(s + " That school is as good as the guy that jumps on the deck and flops like a fish");
+}
+private void skill4() {
+String x = JOptionPane.showInputDialog("what city do you live in?");
+if(x.equalsIgnoreCase("San Diego")) {
+	JOptionPane.showInputDialog("You live in americas finest city");
+}
+else {
+	JOptionPane.showInputDialog("Move to san diego");
+	String e = JOptionPane.showInputDialog("how many cars do you/your family have");
+	int cars = Integer.parseInt(e);
+	if(cars <= 0) {
+		JOptionPane.showInputDialog("you must use public transportation.");
+	}
+	if(cars == 1) {
+JOptionPane.showInputDialog("here is free tesla.");
+	}
+	if(cars > 1) {
+		JOptionPane.showInputDialog("would thst be 8, 10 or 12 wheels, but this isnt question.");
+	}
+}
+}
 private void skill3() {
 Random ran = new Random();
 int q = ran.nextInt(20);
 System.out.println(q);	
 Random ra = new Random();
-int w = ra.nextInt(20);
+int w = ra.nextInt(10);
 System.out.println(w);	
+JOptionPane.showInputDialog(q-w);
 }
 
 void skill2() {
